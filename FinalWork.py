6b164c90493b907aa71b38c07cb637602b004cdd
Sonic_013 <les-scot@mail.ru>
2023-10-23 23:10:50 +0700

    final version

--- FinalWork.py
+++ FinalWork.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
-
+from numpy import int8
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+dumm = pd.DataFrame()
+for column in data.columns:
+  for unique_value in data[column].unique():
+    dumm[column + '_' + unique_value] = (data[column] == unique_value).astype(int8)
+print(dumm)
